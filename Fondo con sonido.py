6011e91d11bedfc28dc9fe11d064dc61f558ae16
Randall-BL <74124774+Randall-BL@users.noreply.github.com>
2020-11-09 11:22:55 -0600

    Sonido en bucle

--- Fondo con sonido.py
+++ Fondo con sonido.py
@@ -1,21 +1,32 @@
-from tkinter import *
-import playsound
 import os
+import threading
+from tkinter import *
+import vlc
+import time
+
+def musica():
+    player = vlc.MediaPlayer("fondo.mp3")
+    player.play()
+    time.sleep(16)
+    return musica()
+
+hiloFondo=threading.Thread(target=musica)
+hiloFondo.start()
+
 def cargarImg(archivo):
-    ruta= os.path.join('img',   archivo)
-    imagen=PhotoImage(file=ruta)
+    ruta = os.path.join('img', archivo)
+    imagen = PhotoImage(file=ruta)
     return imagen
-def reproducir():
-    playsound('Sonido/fondo.mp3')
-    return reproducir()
-reproducir()
-ventana=Tk()
+
+ventana = Tk()
 ventana.title("Avatars vs rooks")
 ventana.minsize(1200, 700)
 ventana.resizable(width=NO, height=NO)
-#Crear imagen de fondo
-C_principal=Canvas(ventana, width=1200, height=700, bg='black')
+C_principal = Canvas(ventana, width=1200, height=700, bg='black')
 C_principal.place(x=0, y=0)
-C_principal.fondo=cargarImg('Tablero1.png')
-imgCanvas= C_principal.create_image(0,0, anchor=NW, image= C_principal.fondo)
+C_principal.fondo = cargarImg('Tablero1.png')
+imgCanvas = C_principal.create_image(0, 0, anchor=NW, image=C_principal.fondo)
+
 ventana.mainloop()
+
+
