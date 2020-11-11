3429097bff86eaf099437a00429d6811b2577767
Randall-BL <74124774+Randall-BL@users.noreply.github.com>
2020-11-10 20:46:28 -0600

    Movimiento Personajes

--- Fondo1.py
+++ Fondo1.py
@@ -1,8 +1,13 @@
 import os
 import threading
+from threading import *
 from tkinter import *
+from tkinter import messagebox
+
 import vlc
 import time
+from time import sleep
+
 
 def musica():
     player = vlc.MediaPlayer("fondo.mp3")
@@ -26,6 +31,25 @@ C_principal = Canvas(ventana, width=1200, height=700, bg='black')
 C_principal.place(x=0, y=0)
 C_principal.fondo = cargarImg('Tablero1.png')
 imgCanvas = C_principal.create_image(0, 0, anchor=NW, image=C_principal.fondo)
+
+C_principal.canibal= cargarImg("canibal.png")
+canibal= C_principal.create_image(165,585,image=C_principal.canibal)
+
+def movimiento():
+    global C_principal
+    cancord= C_principal.coords(canibal)
+    sleep(0.5)
+    if(cancord[1] > 200):
+        C_principal.coords(canibal,cancord[0],cancord[1]-92 )
+    else:
+        return messagebox.showwarning("MALA SUERTE!!", "HAS PERDIDO! ")
+    Thread(target=movimiento, args=()).start()
+
+Thread(target=movimiento, args=()).start()
+
+
+
+
 
 ventana.mainloop()
 
--- PersonajesPrueba1.py
+++ PersonajesPrueba1.py
@@ -1,30 +1,74 @@
-from tkinter import *
-from playsound import playsound
 import os
+from threading import Thread
+from tkinter import *
+import vlc
+import time
+
+player = vlc.MediaPlayer("fondo.mp3")
+
 def cargarImg(archivo):
     ruta= os.path.join('img',   archivo)
     imagen=PhotoImage(file=ruta)
     return imagen
 
-playsound('Sonido\sonido_fondo.mp3')
+
+def juego():
+    ventana.withdraw()
+    ventana1 = Toplevel()
+    ventana1.title("Avatars vs rooks")
+    ventana1.minsize(1200, 700)
+    ventana1.resizable(width=NO, height=NO)
+    #Crear imagen de fondo
+    C_principal=Canvas(ventana1, width=1200, height=700, bg='black')
+    C_principal.place(x=0, y=0)
+    C_principal.fondo=cargarImg('Tablero1.png')
+    imgCanvas1 = C_principal.create_image(0, 0, anchor=NW, image=C_principal.fondo)
+    Imagen1 = cargarImg("Canibal.png")
+    C_principal.image1 = Imagen1
+    canibal = C_principal.create_image(81, 540, anchor=NW, image=C_principal.image1)
+
+    def movimiento():
+        cardcord=C_principal.coords(canibal)
+        if (cardcord(0)<600):
+            C_principal.coords(canibal,carcord(0)+20,cardcord(1))
+        Tk().after(100,movimiento())
+
+    Thread(target=movimiento(),args=()).start()
+
+
+
+    def musica():
+        global player
+        player.play()
+        time.sleep(16)
+        player.stop()
+        return musica()
+
+    hiloFondo = threading.Thread(target=musica)
+    hiloFondo.start()
+
+
+    def mute():
+        global player
+        player.pause()
+
+    sumadre = Button(C_principal, width=8, height=4, text="Pausa/Play", command=mute).place(x=600,y=600)
+
+    ventana1.mainloop()
+
+
+
 ventana=Tk()
 ventana.title("Avatars vs rooks")
 ventana.minsize(1200, 700)
 ventana.resizable(width=NO, height=NO)
 #Crear imagen de fondo
-C_principal=Canvas(ventana, width=1200, height=700, bg='black')
-C_principal.place(x=0, y=0)
-C_principal.fondo=cargarImg('Tablero1.png')
-imgCanvas= C_principal.create_image(0,0, anchor=NW, image= C_principal.fondo)
+C_menu=Canvas(ventana, width=1200, height=700, bg='black')
+C_menu.place(x=0, y=0)
+C_menu.fondo=cargarImg('menu.png')
+imgCanvas= C_menu.create_image(0,0, anchor=NW, image= C_menu.fondo)
 
-def personaje(archivo):
-    ruta= os.path.join('Avatars',   archivo)
-    imagen=PhotoImage(file=ruta)
-    return imagen
-Avatars=Canvas(ventana,width=300, height=300, )
-Avatars.place(x=20, y=20)
-Avatars.Canibal=personaje('Canibal.png')
-imgCanvas=Avatars.create_image(20,20, anchor=NW, image=Avatars.Canibal)
+Playjuego=Button(C_menu,width=8,height=4,text="Inicio",command=juego).place(x=600, y=600)
 
 
 
